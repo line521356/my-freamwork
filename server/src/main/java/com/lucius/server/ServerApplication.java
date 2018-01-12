0ac46393e80bb27580c6543595518525eead0843
“lucius <lucius.li@ixiaoshuidi.com>
2018-01-12 13:20:13 +0800

    freamwork

--- server/src/main/java/com/lucius/server/ServerApplication.java
+++ server/src/main/java/com/lucius/server/ServerApplication.java
@@ -2,8 +2,15 @@ package com.lucius.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.ComponentScan;
+/**
+ *
+ * @author Lucius
+ * create by 2017/10/30
+ * @Emial lucius_521356@163.com
+ */
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.lucius"})
 public class ServerApplication {
 
 	public static void main(String[] args) {
